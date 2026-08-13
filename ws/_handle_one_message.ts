e75ac8e99bad0f0b574ca501ff1e01641ac69921
Nik <nik.moiseienko@gmail.com>
2026-08-14 02:07:58 +1000

    handle one message

---
+++ ws/_handle_one_message.ts
@@ -0,0 +1,22 @@
+export function handle_one_message<D>(
+  ws: WebSocket,
+  { predicat, strict_next_only }: {
+    predicat: (j_data: D) => boolean;
+    strict_next_only?: boolean;
+  },
+) {
+  const { promise, reject, resolve } = Promise.withResolvers();
+  const cb = (ev: MessageEvent<string>) => {
+    const data = JSON.parse(ev.data);
+    if (!predicat(data)) {
+      strict_next_only && reject("if (!predicat(data))");
+      return;
+    }
+    resolve(data);
+    ws.removeEventListener("message", cb);
+  };
+
+  ws.addEventListener("message", cb);
+
+  return promise;
+}
