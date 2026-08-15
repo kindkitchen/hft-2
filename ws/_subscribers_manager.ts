bcf007a7fa2a7bc8105324ba49778079c59f72ab
Nik <nik.moiseienko@gmail.com>
2026-08-16 00:52:46 +1000

    subscription_manager -> rm first draft and make improvment mvp version

--- ws/_subscribers_manager.ts
+++ ws/_subscribers_manager.ts
@@ -1,121 +1,103 @@
 import { __handle_one_message } from "./__handle_one_message.ts";
 import { ____string_with_symbols_tail } from "./__string_with_symbols_tail.ts";
 
-export function _subscriber_manager(ws: WebSocket) {
-  const subscribers: Subscribers = new Map();
-  const symbols_dict = {} as Record<string, Record<string, number>>;
-  const symbol_in_connection_string_map = new Map<string, string>();
+type Subscriber_In =
+  & {
+    topic: string;
+  }
+  & (
+    | {
+      is_parsed_data_expected: true;
+      on_data: <T extends object>(
+        d: T,
+      ) => void;
+    }
+    | {
+      is_parsed_data_expected: false;
+      on_data: (d: string) => void;
+    }
+  );
+type Subscriber = Subscriber_In & {};
 
-  return {
-    subscribers,
-    subscribe: async (
-      subscriber: SubscriberInput<any, any>,
-    ): Promise<SubscriberOutput<any>> => {
+export function _subscribers_manager(ws: WebSocket) {
+  let state_topic_symbols_set = new Set<string>();
+  const subscribers: Map<string, Subscriber[]> = new Map();
+  const build_unsubscribe =
+    (subscriber: Subscriber_In) => async (full_raw_topic?: string) => {
       const {
         topic,
         symbols,
         symbols_set,
-      } = ____string_with_symbols_tail.parse(subscriber.topic);
-      if (!symbols_dict[topic]) {
-        symbols_dict[topic] = {};
-      }
-      for (const s of symbols) {
-        if (symbols_dict[topic][s]) {
-          ++symbols_dict[topic][s];
-        } else {
-          symbols_dict[topic][s] = 1;
+        topic_symbols,
+        topic_symbols_set,
+      } = ____string_with_symbols_tail.parse(
+        full_raw_topic || subscriber.topic,
+      );
+
+      const unsubscribe_symbols = [] as string[];
+      for (const ts of topic_symbols_set) {
+        const group = subscribers.get(ts)!;
+        const i = group.findIndex((s) => s === subscriber);
+        void group.splice(i, 1);
+        if (group.length === 0) {
+          subscribers.delete(ts);
+          state_topic_symbols_set.delete(ts);
+          unsubscribe_symbols.push(ts);
         }
       }
-      const symbols_in_work = new Set(Object.keys(symbols_dict[topic]));
-      const fresh = symbols_set.difference(symbols_in_work);
-
-      for (const s of symbols_set) {
-        const group_name = topic + s;
-        const group = subscribers.get(group_name) || [];
-        group.push(subscriber);
-      }
-      if (fresh.size) {
-        const fresh_arr = fresh.values().toArray();
-        const topic_connection_string = ____string_with_symbols_tail.stringify(
+      const unsubscribe_ack_id = crypto.randomUUID();
+      ws.send(JSON.stringify({
+        id: unsubscribe_ack_id,
+        response: true,
+        type: "unsubscribe",
+        topic: ____string_with_symbols_tail.stringify(
           topic,
-          fresh_arr,
-        );
-        for (const f of fresh) {
-          symbol_in_connection_string_map.set(f, topic_connection_string);
-        }
-        const id = crypto.randomUUID();
-        ws.send(JSON.stringify({
-          id,
-          type: "subscribe",
-          response: true,
-          private: false, /// TODO
-          topic: topic_connection_string,
-        }));
-        await __handle_one_message(ws, {
-          strict_next_only: false,
-          predicat: (j_data) => {
-            return (j_data as { id: string }).id === id;
-          },
-        });
-      }
+          unsubscribe_symbols,
+        ),
+      }));
+      await __handle_one_message(ws, {
+        strict_next_only: false,
+        predicat: ({ id }: { id: string }) => id === unsubscribe_ack_id,
+      });
+    };
+  const subscribe = async (subscriber: Subscriber_In) => {
+    const {
+      topic,
+      symbols,
+      symbols_set,
+      topic_symbols,
+      topic_symbols_set,
+    } = ____string_with_symbols_tail.parse(subscriber.topic);
+    const fresh_topic_symbols_set = topic_symbols_set.difference(
+      state_topic_symbols_set,
+    );
+    for (const fts of fresh_topic_symbols_set) {
+      subscribers.set(fts, []);
+    }
+    for (const ts of topic_symbols) {
+      subscribers.get(ts)!.push(subscriber);
+    }
+    state_topic_symbols_set = state_topic_symbols_set.union(
+      fresh_topic_symbols_set,
+    );
+    const unsubscribe = build_unsubscribe(subscriber);
+    const subscribe_ack_id = crypto.randomUUID();
+    ws.send(JSON.stringify({
+      id: subscribe_ack_id,
+      response: true,
+      type: "subscription",
+      topic: ____string_with_symbols_tail
+        .stringify(topic, fresh_topic_symbols_set.values().toArray()),
+    }));
 
-      return {
-        replace_on_data: (on_data) => subscriber.on_data = on_data,
-        unsubscribe: async () => {
-          for (const s of symbols_set) {
-            const group_name = topic + s;
-            const group = subscribers.get(group_name);
-            if (!group) {
-              console.warn("hmm... may be you already unsubscribed?");
-              continue;
-            }
-            const self_position = group.findIndex((item) =>
-              item === subscriber
-            );
-            if (self_position === -1) {
-              console.warn("hmm... where is your logic?");
-              continue;
-            }
-            void group.splice(self_position, 1);
+    return {
+      unsubscribe,
+    };
+  };
 
-            --symbols_dict[topic][s];
-            if (symbols_dict[topic][s] === 0) {
-              delete symbols_dict[topic][s];
-              if (Object.keys(symbols_dict[topic]).length === 0) {
-                delete symbols_dict[topic];
-              }
-              const id = crypto.randomUUID();
-              ws.send(JSON.stringify({
-                id,
-                type: "unsubscribe",
-                response: true,
-                private: false, /// TODO
-                topic: ____string_with_symbols_tail.stringify(topic, [s]),
-              }));
-              await __handle_one_message(ws, {
-                strict_next_only: false,
-                predicat: (j_data) => {
-                  return (j_data as { id: string }).id === id;
-                },
-              });
-            }
-          }
-        },
-      };
-    },
+  return {
+    subscribe,
+    _subscribers: subscribers,
+    _state_topic_symbols_set: state_topic_symbols_set,
   };
 }
-
-type Subscribers = Map<string, SubscriberInput<any, any>[]>;
-
-type SubscriberInput<T extends boolean, U = object | string> = {
-  topic: string;
-  is_parsed_data_expected: T;
-  on_data: (data: T extends true ? Exclude<U, string> : string) => void;
-};
-
-type SubscriberOutput<U extends object | string> = {
-  // sys: AsyncGenerator<"disconnect" | "reconnect" | "error">;
-  unsubscribe: () => void;
-  replace_on_data: (on_data: (data: U) => void) => void;
-};
